82c8f64ed8983b004218008c6052576cab76bb1b
Alex <hyp3rfury@gmail.com>
2018-01-02 00:05:48 +0100

    Update for.example.js

--- for.example.js
+++ for.example.js
@@ -1,3 +1,17 @@
+// intermediate level
+var arr = [1, "aw", 2, 3, 4];
+// this loop is intended for "queue" purpose
+// used for awaiting anync stuff xD
+// don't get ... it's just an example
+for$(function(v, i, next){
+	var h = new XMLHttpRequest();
+	h.open("GET", "?id=" + v);
+	h.onload = function(){
+		next();
+	};
+	h.send();
+}, arr);
+
 // basic example
 var arr = [1, "aw", 2, 3, 4];
 for$(function(v, i, next){
