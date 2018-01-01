b71267d80e82bca8c410776b988d86b1719209e1
Alex <hyp3rfury@gmail.com>
2018-01-01 23:57:22 +0100

    Update for.js

--- for.js
+++ for.js
@@ -1,2 +1,2 @@
-function loop(n){function o(n){return n instanceof Function}var t=0;!function e(c,i){(c=t-1)!=n.L.length?o(n.ban)&&n.ban(i,t)?e(c,n.L[t++]):n.F(i,c,function(i){i&&o(n.end)?n.end(c,!0,n.L):e(c,n.L[t++])},function(){o(n.end)&&n.end(c,!0,n.L)}):n.end(c,!1,n.L)}(t,n.L[t++])}
-function for$(f,a,i,l){loop({L:a,F:f,ban:i,end:l});}
+function loop(n){function o(n){return n instanceof Function}var t=0;!function e(c,i){(c=t-1)!=n.L.length?n.F(i,c,function(i){!1===i&&o(n.end)?n.end(c,!0,n.L):!1!==i&&e(c,n.L[t++])}):o(n.end)&&n.end(c,!1,n.L)}(t,n.L[t++])}
+function for$(f,u,n){loop({L:u,F:f,end:n})}
