7eb6624303befb1e144ac04a976efe5f74c2bc5a
Alex <hyp3rfury@gmail.com>
2018-01-11 00:17:50 +0100

    Update for2i.example.js

--- for2i.example.js
+++ for2i.example.js
@@ -9,7 +9,15 @@ for$([1, 2, 3], function(o){
 	else o.go();
 });
 
-// fast for numbers
+// string version
+for$("abc", function(o){
+	console.log(o.i, o.v);
+	if (o.e)
+		console.log("loop ended 2");
+	else o.go();
+});
+
+// fast for numbers, out: 0,1,2,3
 iter(0, 4, function(o){
 	console.log(o.v);
 	if (o.e)
